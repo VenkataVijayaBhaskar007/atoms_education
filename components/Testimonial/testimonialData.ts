54c454a0abab45b3cc35c01dc4929a80b37303fe
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2024-01-08 16:06:37 +0530

    added tesitmonials

--- components/Testimonial/testimonialData.ts
+++ components/Testimonial/testimonialData.ts
@@ -33,30 +33,30 @@ export const testimonialData = [
     designation: "Student @ VJ Degree College",
     image: image2,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "Amazing experience with the team and classes, good communication skills with values and morals. Placement training program helped me a lot in choosing better placemnt and also with real time experienced persons.",
   },
   {
     id: 4,
-    name: "Jhon Abraham",
-    designation: "Founter @democompany",
+    name: "Abdul",
+    designation: "Student @ VVIT",
     image: image2,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "Trust me these guys were really awesome, their values, skills and effort to educate and experience any thing that they have were really advanced than the outer mediums we were getting...",
   },
   {
     id: 4,
-    name: "Jhon Abraham",
-    designation: "Founter @democompany",
+    name: "Naveen Kumar",
+    designation: "Trainee @ Acumen Club",
     image: image2,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "I am Naveen Kumar, currently a trainee at the Acumen Club. I'd like to share my experiences of my time here. It has been a month since I joined this club. Nowadays, effective communication is undeniably a necessary skill.",
   },
   {
     id: 4,
-    name: "Jhon Abraham",
-    designation: "Founter @democompany",
+    name: "Damini",
+    designation: "Student @ KITS",
     image: image2,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "At the begining I never belived that this is gonna workout for me. but trust me this is definitely a better option for any one to choose and upgrade themselves for placement and english communication skills. So, don't waste your time and join ASAP!",
   },
 ];
